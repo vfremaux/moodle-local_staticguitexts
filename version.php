0583c5c37191a7e7ad5ac63a0f48b51d9fe9b8c4
valery <valery.fremaux@gmail.com>
2016-10-11 01:12:38 +0200

    Automated sync from SVN

--- version.php
+++ version.php
@@ -14,13 +14,23 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+/**
+ * Version details.
+ *
+ * @package     local_staticguitexts
+ * @category    local
+ * @author      Valery Fremaux <valery.fremaux@gmail.com>
+ * @copyright   2009 onwards Valery Fremaux (http://www.mylearningfactory.com)
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2013121900;   // The (date) version of this plugin
-$plugin->requires = 2011120500;   // Requires this Moodle version
+$plugin->version  = 2013121900;   // The (date) version of this plugin.
+$plugin->requires = 2014111000;   // Requires this Moodle version.
 $plugin->component = 'local_staticguitexts';
-$plugin->release = '2.7.0 (Build 2013121900)';
-$plugin->maturity = MATURITY_RC;
+$plugin->release = '2.8.0 (Build 2013121900)';
+$plugin->maturity = MATURITY_STABLE;
 
-// Non moodle attributes
-$plugin->codeincrement = '2.7.0000';
+// Non moodle attributes.
+$plugin->codeincrement = '2.8.0000';
