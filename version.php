486262a0ae99b36517aa1244f408ad05365fe4cc
valery <valery.fremaux@gmail.com>
2018-03-24 20:03:34 +0100

    fix travis

--- version.php
+++ version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version  = 2013121900;   // The (date) version of this plugin.
-$plugin->requires = 2015072400;   // Requires this Moodle version.
+$plugin->requires = 2016120500;   // Requires this Moodle version.
 $plugin->component = 'local_staticguitexts';
-$plugin->release = '3.1.0 (Build 2013121900)';
+$plugin->release = '3.2.0 (Build 2013121900)';
 $plugin->maturity = MATURITY_STABLE;
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.1.0000';
+$plugin->codeincrement = '3.2.0000';
